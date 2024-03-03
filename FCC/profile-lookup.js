88643b3e6544ed84f8b4551e57c137188e297296
divyaprakash <d.prakash1506@gmail.com>
2024-03-03 11:50:46 +0530

    profile-lookup update

--- FCC/profile-lookup.js
+++ FCC/profile-lookup.js
@@ -1,50 +1,54 @@
 // Setup
 const contacts = [
-    {
-      firstName: "Akira",
-      lastName: "Laine",
-      number: "0543236543",
-      likes: ["Pizza", "Coding", "Brownie Points"],
-    },
-    {
-      firstName: "Harry",
-      lastName: "Potter",
-      number: "0994372684",
-      likes: ["Hogwarts", "Magic", "Hagrid"],
-    },
-    {
-      firstName: "Sherlock",
-      lastName: "Holmes",
-      number: "0487345643",
-      likes: ["Intriguing Cases", "Violin"],
-    },
-    {
-      firstName: "Kristian",
-      lastName: "Vos",
-      number: "unknown",
-      likes: ["JavaScript", "Gaming", "Foxes"],
-    },
-  ];
-  
-  function lookUpProfile(name, prop) {
-    // Only change code below this line
-    let noSuchName=true;
-    let noSuchProp = true;
-    for(let i=0;i<contacts.length;i++){
-        if(contacts[i]["firstName"]===name && contacts[i].hasOwnProperty(prop)){
-            return contacts[i][prop];
-        }else if(contacts[i]["firstName"] ===name){
-            noSuchName = false;
-        }else if(contacts[i].hasOwnProperty(prop)){
-            noSuchProp = false;
-        }
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  let noSuchName = true;
+  let noSuchProp = true;
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i]["firstName"] === name && contacts[i].hasOwnProperty(prop)) {
+      return contacts[i][prop];
+    } else if (contacts[i]["firstName"] === name) {
+      noSuchName = false;
+    } else if (contacts[i].hasOwnProperty(prop)) {
+      noSuchProp = false;
     }
-    if(noSuchName){
-        return "No such contact";
-    }else if(noSuchProp){
-        return "No such property";
-    }
-    // Only change code above this line
   }
-  
-  lookUpProfile("Akira", "likes");
+  if (noSuchName) {
+    return "No such contact";
+  } else if (noSuchProp) {
+    return "No such property";
+  }
+  // Only change code above this line
+}
+function lookUpProfile2(name, prop) {
+  return contacts.map((items) =>
+    items.firstName === name && items.hasOwnProperty(prop) ? true : false
+  );
+}
+console.log(lookUpProfile2("Akira", "likes"));
